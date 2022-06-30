a04a9a22db8965fbb114de9c519e2b3b4a7bdb82
Facundo Beloqui <facu.beloqui04@gmail.com>
2022-06-30 10:36:27 -0300

    boton agregado

--- src/components/Formulario.js
+++ src/components/Formulario.js
@@ -3,6 +3,8 @@ import { Formik, Field, Form } from 'formik';
 import Input from '@mui/material/Input';
 import { TextField } from '@mui/material';
 import Box from '@mui/material/Box';
+import * as yup from 'yup'
+import Button from '@mui/material/Button';
 
 
 const Validacion = yup.object({
@@ -16,23 +18,31 @@ const Validacion = yup.object({
 export class Formulario extends Component {
   render() {
     return (
+      
       <div>
           <Box
           sx={{
             display: 'flex',
             alignItems: 'center',
             '& > :not(style)': { m: 1 },
-            justifyContent: 'center'
+            justifycontent: 'center'
           }} >
             <TextField 
             helperText="Ingrese un email"
             id="email"
             label="Email"/>
-
             
+          <Button variant='Contained' justifyContent='center' onClick={Validacion}>Enviar</Button>
 
           </Box>
+
+          
+
+          
+
+          
       </div>
+      
     )
   }
 }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,11 @@
 import React from 'react'
 import Formulario from '../components/Formulario' 
-import Cita from '../components/Cita'
-import ListadoCitas from '../components/ListadoCitas'
+
 
 export default function Home() {
   return (
-    <div>
+    <>
         <Formulario />
-    </div>
+    </>
   )
 }
