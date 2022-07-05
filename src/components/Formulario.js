4c15a23864d7551392083296de89dbd83918b2bd
facubeloqui <facu.beloqui04@gmail.com>
2022-07-05 10:34:19 -0300

    Utilizando Hooks

--- src/components/Formulario.js
+++ src/components/Formulario.js
@@ -1,50 +1,40 @@
-import React, { Component } from 'react'
-import { Formik, Field, Form } from 'formik';
-import Input from '@mui/material/Input';
-import { TextField } from '@mui/material';
-import Box from '@mui/material/Box';
-import * as yup from 'yup'
-import Button from '@mui/material/Button';
+import React, {Fragment, useState} from 'react'
 
+function Formulario() {
+  const [datos, setDatos] = useState({
+    nombre: '',
+    apellido: ''
+  })
 
-const Validacion = yup.object({
-    email: yup
-    .string('Ingresa tu mail')
-    .email('Ingresa un email valido')
-    .required('De esta no safas')
-})
-
-
-export class Formulario extends Component {
-  render() {
-    return (
-      
-      <div>
-          <Box
-          sx={{
-            display: 'flex',
-            alignItems: 'center',
-            '& > :not(style)': { m: 1 },
-            justifycontent: 'center'
-          }} >
-            <TextField 
-            helperText="Ingrese un email"
-            id="email"
-            label="Email"/>
-            
-          <Button variant='Contained' justifyContent='center' onClick={Validacion}>Enviar</Button>
-
-          </Box>
-
-          
-
-          
-
-          
-      </div>
-      
-    )
-  }
+  const handleInputChange = (event) => {
+    // console.log(event.target.name)
+    // console.log(event.target.value)
+    setDatos({
+        ...datos,
+        [event.target.name] : event.target.value
+    })
 }
-
+const enviarDatos = (event) => {
+  event.preventDefault()
+  console.log('enviando datos...' + datos.nombre + ' ' + datos.apellido)
+}
+  return (
+    <Fragment>
+            <h1>Formulario</h1>
+            <form className="row" onSubmit={enviarDatos}>
+                <div className="col-md-3">
+                    <input type="text" placeholder="Nombre" className="form-control" onChange={handleInputChange} name="nombre"></input>
+                </div>
+                <div className="col-md-3">
+                    <input type="text" placeholder="Apellido" className="form-control" onChange={handleInputChange} name="apellido"></input>
+                </div>
+                <button type="submit" className="btn btn-primary">Enviar</button>
+            </form>
+            <ul>
+                <li>{datos.nombre}</li>
+                <li>{datos.apellido}</li>
+            </ul>
+        </Fragment>
+  )
+}
 export default Formulario
