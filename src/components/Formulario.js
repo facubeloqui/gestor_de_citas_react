bc6f085ba617503edcfbae9936308d6dcbfa6242
Facundo Beloqui <facu.beloqui04@gmail.com>
2022-07-06 12:57:38 -0300

    maquetado del formulario terminado

--- src/components/Formulario.js
+++ src/components/Formulario.js
@@ -2,8 +2,11 @@ import React, {Fragment, useState} from 'react'
 
 function Formulario() {
   const [datos, setDatos] = useState({
-    nombre: '',
-    apellido: ''
+    nombreMascota: '',
+    nombreDueno: '',
+    fecha: '',
+    hora: '',
+    sintomas: ''
   })
 
   const handleInputChange = (event) => {
@@ -16,17 +19,26 @@ function Formulario() {
 }
 const enviarDatos = (event) => {
   event.preventDefault()
-  console.log('enviando datos...' + datos.nombre + ' ' + datos.apellido)
+  console.log('enviando datos...' + datos.nombreMascota + ' ' + datos.nombreDueno + ' ' + datos.fecha + ' ' + datos.hora  + ' ' + datos.sintomas)
 }
   return (
     <Fragment>
-            <h1>Formulario</h1>
+            <h1>Crear mi Cita</h1>
             <form className="row" onSubmit={enviarDatos}>
                 <div className="col-md-3">
-                    <input type="text" placeholder="Nombre" className="form-control" onChange={handleInputChange} name="nombre"></input>
+                    <input type="text" placeholder="Nombre de la Mascota" className="form-control" onChange={handleInputChange} name="nombreMascota"></input>
                 </div>
                 <div className="col-md-3">
-                    <input type="text" placeholder="Apellido" className="form-control" onChange={handleInputChange} name="apellido"></input>
+                    <input type="text" placeholder="Nombre del dueño" className="form-control" onChange={handleInputChange} name="nombreDueño"></input>
+                </div>
+                <div className="col-md-3">
+                    <input type="date" placeholder="Fecha" className="form-control" onChange={handleInputChange} name="fecha"></input>
+                </div>
+                <div className="col-md-3">
+                    <input type="time" placeholder="Hora" className="form-control" onChange={handleInputChange} name="hora"></input>
+                </div>
+                <div className="col-md-3">
+                    <textarea  className="form-control" onChange={handleInputChange} name="texto"></textarea>
                 </div>
                 <button type="submit" className="btn btn-primary">Enviar</button>
             </form>
